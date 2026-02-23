3f7bbb2642fa0f4506d684ce6ad926c8e2256ae4
da8462-aj <da8462@srmist.edu.in>
2026-02-23 11:06:12 +0530

    Fix: corrected palindrome logic

--- APP/src/PallindroneCheckerApp.java
+++ APP/src/PallindroneCheckerApp.java
@@ -1,33 +1,24 @@
 // USE CASE 3
 import java.util.Scanner;
-
 public class PallindroneCheckerApp {
-    public static void main(String[] args) {
+     public static void main(String[] args) {
+         Scanner sc=new Scanner(System.in);
+         String s;
+         System.out.println("Welcome to Palindrone Checker App Management System");
+         System.out.println("Enter a string(lower case):");
+         s=sc.nextLine();
+         String text="";
+     for (int i=s.length()-1;i>=0;i--){
+         text=text + s.charAt(i);
+     }
+     if(s.equals(text)){
+         System.out.println(" Palindrone");
+     }else{
+         System.out.println(" Not Palindrone");
 
-        Scanner sc = new Scanner(System.in);
+     }
+     sc.close();
 
-        System.out.println("Welcome to Palindrone Checker App Management System");
-        System.out.println("Enter a string (lower case):");
 
-        String s = sc.nextLine();
-        boolean isPalindrome = true;
-
-        // Loop only till half of the string length
-        for (int i = 0; i < s.length() / 2; i++) {
-
-            // Compare characters from start and end
-            if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
-                isPalindrome = false;
-                break;
-            }
-        }
-
-        if (isPalindrome) {
-            System.out.println("Palindrome");
-        } else {
-            System.out.println("Not Palindrome");
-        }
-
-        sc.close();
     }
 }
