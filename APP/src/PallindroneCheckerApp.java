2772416c687a043d26160a6fba47fb6fd385077b
siddharthkumar140206-web <siddharthkumar140206@gmail.com>
2026-03-02 10:58:17 +0530

    Sidharth added UC-13PerformanceComparison

--- APP/src/PallindroneCheckerApp.java
+++ APP/src/PallindroneCheckerApp.java
@@ -1,4 +1,4 @@
-// USE CASE 12
+// USE CASE 13
 import java.util.Scanner;
 import java.util.ArrayDeque;
 import java.util.Deque;
@@ -11,25 +11,52 @@ public class PallindroneCheckerApp {
 
         System.out.println("Enter a string:");
         String input = sc.nextLine();
-        PalindromeStrategy strategy = new DequeStrategy();
-        boolean result = strategy.check(input);
+        input = input.toLowerCase().replaceAll("[^a-z0-9]", "");
 
-        System.out.println("Is Palindrome?: " + result);
+        long startRecursive = System.nanoTime();
+
+        boolean recursiveResult = recursiveCheck(input, 0, input.length() - 1);
+
+        long endRecursive = System.nanoTime();
+        long recursiveTime = endRecursive - startRecursive;
+
+        long startDeque = System.nanoTime();
+
+        boolean dequeResult = dequeCheck(input);
+
+        long endDeque = System.nanoTime();
+        long dequeTime = endDeque - startDeque;
+
+
+        System.out.println("\nRecursive Result: " + recursiveResult);
+        System.out.println("Recursive Execution Time: " + recursiveTime + " ns");
+
+        System.out.println("\nDeque Result: " + dequeResult);
+        System.out.println("Deque Execution Time: " + dequeTime + " ns");
 
         sc.close();
     }
-}
-interface PalindromeStrategy {
-    boolean check(String input);
-}
-class DequeStrategy implements PalindromeStrategy {
 
-    public boolean check(String input) {
+
+    private static boolean recursiveCheck(String s, int start, int end) {
+
+        if (start >= end)
+            return true;
+
+        if (s.charAt(start) != s.charAt(end))
+            return false;
+
+        return recursiveCheck(s, start + 1, end - 1);
+    }
+
+    private static boolean dequeCheck(String input) {
 
         Deque<Character> deque = new ArrayDeque<>();
+
         for (char c : input.toCharArray()) {
             deque.addLast(c);
         }
+
         while (deque.size() > 1) {
 
             if (!deque.removeFirst().equals(deque.removeLast())) {
